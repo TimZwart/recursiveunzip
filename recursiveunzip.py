89e693e59ed9a767fc54c367d9d21d3ca1252a29
Tim Zwart <Tim.Zwart@vixion.nl>
2017-09-06 16:29:07 +0200

    now sorta works

--- recursiveunzip.py
+++ recursiveunzip.py
@@ -1,12 +1,23 @@
+#!/bin/python
 import os;
 import subprocess;
 
 print 'unzipping all .src.zip files'
 
-for (dirpath, dirnames, filenames) in os.walk(folder):
+for (dirpath, dirnames, filenames) in os.walk('.'):
     for filename in filenames:
         if filename.endswith(".src.zip"):
             filename_minuszip = filename[:-4]
-            os.chdir(dirpath)
             if not os.path.isfile(filename_minuszip):
-                subprocess.call('unzip '+filename+' -d '+filename_minuszip)
+                fullpath = "/".join([os.getcwd(), dirpath, filename])
+                fullpath_minuszip = fullpath[:-4]
+                com = 'unzip '+fullpath+' -d '+fullpath_minuszip
+                print "command, $", com
+                com2 = 'pwd'
+                print "working dir according to subprocess, pwd"
+                subprocess.call(com2, cwd=dirpath)
+                print "file call"
+                com3 = 'file '+fullpath
+                print com3
+                subprocess.call(com3, shell=True)
+                subprocess.call(com, shell=True)
